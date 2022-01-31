aa5ee55059c87f9814b7bf22e23e2cb9781f4644
Jef Roosens <roosensjef@gmail.com>
2022-01-31 23:39:04 +0100

    Tweaked CI a bit

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def create_random_pkginfo(words, name_min_len, name_max_len):
     Generates a random .PKGINFO
     """
     name = "-".join(random_words(words, name_min_len, name_max_len))
-    ver = "0.1.0"  # doesn't matter what it is anyways
+    ver = "0.1.0-1"  # doesn't matter what it is anyways
 
     # TODO add random dependencies (all types)
 
