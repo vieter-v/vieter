cb492cadc6ba24321ecc7153d21e00c7c5838057
Jef Roosens <roosensjef@gmail.com>
2022-01-31 14:19:37 +0100

    Added builder patch for public sha256 functions

--- patches/patch.sh
+++ patches/patch.sh
@@ -5,3 +5,9 @@
 
 # Add parse_request_no_body
 cat parse_request_no_body.v >> "$1"/vlib/net/http/request.v
+
+# Make sha256 functions public
+sed -i \
+    -e 's/\(fn (mut d Digest) checksum(\)/pub \1/' \
+    -e 's/\(fn (mut d Digest) write(\)/pub \1/' \
+    "$1"/vlib/crypto/sha256/sha256.v
