78310fa1e425652fb0f2f2e29865a6f2b9310cf0
Jef Roosens <roosensjef@gmail.com>
2022-04-07 12:10:37 +0200

    Builder now uses new Git repos API

--- src/git/client.v
+++ src/git/client.v
@@ -4,7 +4,8 @@ import json
 import response { Response }
 import net.http
 
-fn get_repos(address string, api_key string) ?map[string]GitRepo {
+// get_repos returns the current list of repos.
+pub fn get_repos(address string, api_key string) ?map[string]GitRepo {
 	mut req := http.new_request(http.Method.get, '$address/api/repos', '') ?
 	req.add_custom_header('X-API-Key', api_key) ?
 
@@ -14,7 +15,8 @@ fn get_repos(address string, api_key string) ?map[string]GitRepo {
 	return data.data
 }
 
-fn add_repo(address string, api_key string, url string, branch string, arch []string) ?Response<string> {
+// add_repo adds a new repo to the server.
+pub fn add_repo(address string, api_key string, url string, branch string, arch []string) ?Response<string> {
 	mut req := http.new_request(http.Method.post, '$address/api/repos?url=$url&branch=$branch&arch=${arch.join(',')}',
 		'') ?
 	req.add_custom_header('X-API-Key', api_key) ?
@@ -25,7 +27,8 @@ fn add_repo(address string, api_key string, url string, branch string, arch []st
 	return data
 }
 
-fn remove_repo(address string, api_key string, id string) ?Response<string> {
+// remove_repo removes the repo with the given ID from the server.
+pub fn remove_repo(address string, api_key string, id string) ?Response<string> {
 	mut req := http.new_request(http.Method.delete, '$address/api/repos/$id', '') ?
 	req.add_custom_header('X-API-Key', api_key) ?
 
