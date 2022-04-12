bd0c276fd84c483a5e9bc73c3c833c6793f04615
Jef Roosens <roosensjef@gmail.com>
2022-04-12 21:28:44 +0200

    Added 'WIP' notice for cron cli

--- src/cron/cron.v
+++ src/cron/cron.v
@@ -14,8 +14,5 @@ fn (r1 ScheduledBuild) < (r2 ScheduledBuild) bool {
 
 // cron starts a cron daemon & starts periodically scheduling builds.
 pub fn cron(conf Config) ? {
-	ce := parse_expression('0 3') ?
-	t := time.parse('2002-01-01 00:00:00') ?
-	t2 := ce.next(t) ?
-	println(t2)
+	println('WIP')
 }
