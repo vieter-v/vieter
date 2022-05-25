7f6e9e636c378978082200e2aa5342c43bba8a51
Jef Roosens <roosensjef@gmail.com>
2022-05-25 09:24:01 +0200

    fix(cron): retrieve all GitRepo's instead of first 25

--- src/client/git.v
+++ src/client/git.v
@@ -4,12 +4,33 @@ import models { GitRepo, GitRepoFilter }
 import net.http { Method }
 import response { Response }
 
-// get_git_repos returns the current list of repos.
+// get_git_repos returns a list of GitRepo's, given a filter object.
 pub fn (c &Client) get_git_repos(filter GitRepoFilter) ?[]GitRepo {
 	params := models.params_from(filter)
 	data := c.send_request<[]GitRepo>(Method.get, '/api/repos', params)?
 
 	return data.data
+}
+
+// get_all_git_repos retrieves *all* GitRepo's from the API using the default
+// limit.
+pub fn (c &Client) get_all_git_repos() ?[]GitRepo {
+	mut repos := []GitRepo{}
+	mut offset := u64(0)
+
+	for {
+		sub_repos := c.get_git_repos(offset: offset)?
+
+		if sub_repos.len == 0 {
+			break
+		}
+
+		repos << sub_repos
+
+		offset += u64(sub_repos.len)
+	}
+
+	return repos
 }
 
 // get_git_repo returns the repo for a specific ID.
