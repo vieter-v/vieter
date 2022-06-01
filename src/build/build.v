48e2ae7645f2de788801185bb096aee11cfbf983
Chewing_Bever <roosensjef@gmail.com>
2022-06-01 20:34:36 +0200

    feat(build): show shell commands in build logs

--- src/build/build.v
+++ src/build/build.v
@@ -101,40 +101,19 @@ pub fn build_repo(address string, api_key string, base_image_id string, repo &Gi
 	}
 
 	build_arch := os.uname().machine
+	build_script := create_build_script(address, repo, build_arch)
 
-	repo_url := '$address/$repo.repo'
-
-	// TODO what to do with PKGBUILDs that build multiple packages?
-	commands := [
-		// This will later be replaced by a proper setting for changing the
-		// mirrorlist
-		"echo -e '[$repo.repo]\nServer = $address/\$repo/\$arch\nSigLevel = Optional' >> /etc/pacman.conf"
-		// We need to update the package list of the repo we just added above.
-		// This should however not pull in a lot of packages as long as the
-		// builder image is rebuilt frequently.
-		'pacman -Syu --needed --noconfirm',
-		'su builder',
-		'git clone --single-branch --depth 1 --branch $repo.branch $repo.url repo',
-		'cd repo',
-		'makepkg --nobuild --syncdeps --needed --noconfirm',
-		'source PKGBUILD',
-		// The build container checks whether the package is already
-		// present on the server
-		'curl -s --head --fail $repo_url/$build_arch/\$pkgname-\$pkgver-\$pkgrel && exit 0',
-		'MAKEFLAGS="-j\$(nproc)" makepkg -s --noconfirm --needed && for pkg in \$(ls -1 *.pkg*); do curl -XPOST -T "\$pkg" -H "X-API-KEY: \$API_KEY" $repo_url/publish; done',
-	]
-
-	// We convert the list of commands into a base64 string, which then gets
-	// passed to the container as an env var
-	cmds_str := base64.encode_str(commands.join('\n'))
+	// We convert the build script into a base64 string, which then gets passed
+	// to the container as an env var
+	base64_script := base64.encode_str(build_script)
 
 	c := docker.NewContainer{
 		image: '$base_image_id'
-		env: ['BUILD_SCRIPT=$cmds_str', 'API_KEY=$api_key']
+		env: ['BUILD_SCRIPT=$base64_script', 'API_KEY=$api_key']
 		entrypoint: ['/bin/sh', '-c']
 		cmd: ['echo \$BUILD_SCRIPT | base64 -d | /bin/bash -e']
 		work_dir: '/build'
-		// user: 'builder:builder'
+		user: '0:0'
 	}
 
 	id := dd.create_container(c)?.id
