3342eedfa45c94d94224f0367ef265bd14ed451b
Chewing_Bever <roosensjef@gmail.com>
2022-12-22 23:10:10 +0100

    chore: compile with -skip-unused

--- src/server/api_jobs.v
+++ src/server/api_jobs.v
@@ -4,7 +4,7 @@ import web
 import web.response { new_data_response, new_response }
 
 // v1_poll_job_queue allows agents to poll for new build jobs.
-['/api/v1/jobs/poll'; auth; get]
+['/api/v1/jobs/poll'; auth; get; markused]
 fn (mut app App) v1_poll_job_queue() web.Result {
 	arch := app.query['arch'] or {
 		return app.json(.bad_request, new_response('Missing arch query arg.'))
@@ -21,7 +21,7 @@ fn (mut app App) v1_poll_job_queue() web.Result {
 }
 
 // v1_queue_job allows queueing a new one-time build job for the given target.
-['/api/v1/jobs/queue'; auth; post]
+['/api/v1/jobs/queue'; auth; markused; post]
 fn (mut app App) v1_queue_job() web.Result {
 	target_id := app.query['target'] or {
 		return app.json(.bad_request, new_response('Missing target query arg.'))
