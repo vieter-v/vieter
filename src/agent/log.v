8432f5915d51ae485313cdd03ea0d9983e1425a9
GreekStapler <dvmap@protonmail.com>
2023-01-07 21:09:55 +0000

    Fix for configured log level being ignored.

--- src/agent/log.v
+++ src/agent/log.v
@@ -1,35 +1,36 @@
 module agent
 
-import log
-
-// log a message with the given level
-pub fn (mut d AgentDaemon) log(msg string, level log.Level) {
-	lock d.logger {
-		d.logger.send_output(msg, level)
-	}
-}
-
 // lfatal create a log message with the fatal level
 pub fn (mut d AgentDaemon) lfatal(msg string) {
-	d.log(msg, log.Level.fatal)
+	lock d.logger {
+		d.logger.fatal(msg)
+	}
 }
 
 // lerror create a log message with the error level
 pub fn (mut d AgentDaemon) lerror(msg string) {
-	d.log(msg, log.Level.error)
+	lock d.logger {
+		d.logger.error(msg)
+	}
 }
 
 // lwarn create a log message with the warn level
 pub fn (mut d AgentDaemon) lwarn(msg string) {
-	d.log(msg, log.Level.warn)
+	lock d.logger {
+		d.logger.warn(msg)
+	}
 }
 
 // linfo create a log message with the info level
 pub fn (mut d AgentDaemon) linfo(msg string) {
-	d.log(msg, log.Level.info)
+	lock d.logger {
+		d.logger.info(msg)
+	}
 }
 
 // ldebug create a log message with the debug level
 pub fn (mut d AgentDaemon) ldebug(msg string) {
-	d.log(msg, log.Level.debug)
+	lock d.logger {
+		d.logger.debug(msg)
+	}
 }
--- src/cron/daemon/log.v
+++ src/cron/daemon/log.v
@@ -1,35 +1,36 @@
 module daemon
 
-import log
-
-// log reate a log message with the given level
-pub fn (mut d Daemon) log(msg string, level log.Level) {
-	lock d.logger {
-		d.logger.send_output(msg, level)
-	}
-}
-
 // lfatal create a log message with the fatal level
 pub fn (mut d Daemon) lfatal(msg string) {
-	d.log(msg, log.Level.fatal)
+	lock d.logger {
+		d.logger.fatal(msg)
+	}
 }
 
 // lerror create a log message with the error level
 pub fn (mut d Daemon) lerror(msg string) {
-	d.log(msg, log.Level.error)
+	lock d.logger {
+		d.logger.error(msg)
+	}
 }
 
 // lwarn create a log message with the warn level
 pub fn (mut d Daemon) lwarn(msg string) {
-	d.log(msg, log.Level.warn)
+	lock d.logger {
+		d.logger.warn(msg)
+	}
 }
 
 // linfo create a log message with the info level
 pub fn (mut d Daemon) linfo(msg string) {
-	d.log(msg, log.Level.info)
+	lock d.logger {
+		d.logger.info(msg)
+	}
 }
 
 // ldebug create a log message with the debug level
 pub fn (mut d Daemon) ldebug(msg string) {
-	d.log(msg, log.Level.debug)
+	lock d.logger {
+		d.logger.debug(msg)
+	}
 }
